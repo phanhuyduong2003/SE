2be3658760ff79b2edace306fadac8009a95dac2
phanhuyduong2003 <huyduongphan8@gmail.com>
2023-03-18 02:50:36 +0700

    CRUD

---
+++ sample_fastapi_mysql_app/SE.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE my_test_db;
+USE my_test_db;
+CREATE TABLE IF NOT EXISTS product(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(1024),
+    price BIGINT DEFAULT 0,
+    is_available BOOLEAN DEFAULT FALSE,
+    seller_email VARCHAR(512),
+    deleted BOOLEAN DEFAULT FALSE,
+    created_by INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_by INT NULL,
+    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE = INNODB;
